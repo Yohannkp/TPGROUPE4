8a6dfb5cd709ed1628f3ee120464fd6c5d421de2
shaykkoo <liyam78830@gmail.com>
2024-03-15 11:15:36 +0100

    check is Auth on article routes

--- app/routes/Article/index.js
+++ app/routes/Article/index.js
@@ -1,12 +1,12 @@
 const productRoute = require('express').Router(),
     productController = require('../../controllers/article');
-const {checkIsAUth} = require("../../config/jwtConfig");
+    const {checkIsAuth} = require("../../config/jwtConfig");
 
 module.exports = (app) => {
-    productRoute.get('/articles', productController.getAll)
-    productRoute.post('/article', productController.create)
-    productRoute.put('/article/:uuid', productController.update)
-    productRoute.delete('/article/:uuid', productController.delete)
-    productRoute.get('/article/:uuid', productController.getById)
+    productRoute.get('/articles', checkIsAuth, productController.getAll)
+    productRoute.post('/article', checkIsAuth, productController.create)
+    productRoute.put('/article/:uuid', checkIsAuth, productController.update)
+    productRoute.delete('/article/:uuid', checkIsAuth, productController.delete)
+    productRoute.get('/article/:uuid', checkIsAuth, productController.getById)
     app.use('/api/v1', productRoute)
 }
